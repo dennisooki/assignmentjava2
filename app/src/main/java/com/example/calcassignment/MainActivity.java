5605eab5c9bb9117a59c7c27d0bb3155b7a599e5
dennisooki <dennisooki@gmail.com>
2023-10-15 19:22:15 +0300

    added layout

---
+++ app/src/main/java/com/example/calcassignment/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.calcassignment;
+
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    EditText inputFirstNumber, inputSecondNumber;
+    Button addButton,subtractButton, divideButton, multiplyButton,resetButton,resultButton;
+    TextView resultView;
+
+
+}
